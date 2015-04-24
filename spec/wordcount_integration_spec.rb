a463d9e940e80143f53e5b01d46ba510abb90b31
Drew <drew@finstrom.us>
2015-04-24 11:09:12 -0700

    add integration test to verify expected output

--- spec/wordcount_integration_spec.rb
+++ spec/wordcount_integration_spec.rb
@@ -4,11 +4,20 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
-describe('Sinatra Test', {:type => :feature}) do
+describe('Wordcount Integration Testing', {:type => :feature}) do
   #framework test
   it('verifies a test route is getting to a test layout page') do
     visit('/test')
     expect(page).to have_content('Sinatra OK')
   end
-  
+
+  #
+  it('returns the expected count when passed a parameter string where matching words include mixed case and punctuation') do
+    visit('/')
+    fill_in('', :with => 'a_string')
+    fill_in('', :with => 'A_string, a_string, - my kingdom for A_STRING!')
+    click_button('Send')
+    expect(page).to have_content("'a_string' appears 3 time(s) in: ")
+  end
+
 end
