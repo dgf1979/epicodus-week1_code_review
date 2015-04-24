80627b38be8a9114deb73c9582b9d779ba708f6a
Drew <drew@finstrom.us>
2015-04-24 11:35:37 -0700

    add view code for entry form and minimal results display, plus route updates to apply method to input

--- app.rb
+++ app.rb
@@ -11,13 +11,13 @@ end
 
 #default route and form page
 get('/') do
-  @erb(:search_form)
+  erb(:search_form)
 end
 
 #search results route
 get('/results') do
-  @word_to_search_for = params.fetch('')
-  @sentance_to_search = params.fetch('')
+  @word_to_search_for = params.fetch('search_word')
+  @sentance_to_search = params.fetch('search_in')
   @word_count = @word_to_search_for.exists_number_of_times_in(@sentance_to_search)
   erb(:search_results)
 end
--- spec/wordcount_integration_spec.rb
+++ spec/wordcount_integration_spec.rb
@@ -14,10 +14,10 @@ describe('Wordcount Integration Testing', {:type => :feature}) do
   #
   it('returns the expected count when passed a parameter string where matching words include mixed case and punctuation') do
     visit('/')
-    fill_in('', :with => 'a_string')
-    fill_in('', :with => 'A_string, a_string, - my kingdom for A_STRING!')
+    fill_in('search_word', :with => 'a_string')
+    fill_in('search_in', :with => 'A_string, a_string, - my kingdom for A_STRING!')
     click_button('Send')
-    expect(page).to have_content("'a_string' appears 3 time(s) in: ")
+    expect(page).to have_content("3")
   end
 
 end
