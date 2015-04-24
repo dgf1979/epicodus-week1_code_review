696c1aab814975d9a9d00ead48c1254e7e76f8af
Drew <drew@finstrom.us>
2015-04-24 09:56:15 -0700

    add method logic to pass equal-word unit test

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,6 +1,13 @@
 class String
   #method to count the # of times this string exists in the parameter string
   define_method(:exists_number_of_times_in) do |sentance_to_search|
-    
+    word_to_locate = self
+
+
+    if word_to_locate == sentance_to_search
+      return 1
+    end
+
+
   end
 end
