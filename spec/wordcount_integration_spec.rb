e04baba6ef977ab62d4bce3d3c9676415a40c22e
Drew <drew@finstrom.us>
2015-04-24 11:41:32 -0700

    add two integration tests to verify that the form input for string to search for and string to search in each exist in results page

--- spec/wordcount_integration_spec.rb
+++ spec/wordcount_integration_spec.rb
@@ -11,13 +11,31 @@ describe('Wordcount Integration Testing', {:type => :feature}) do
     expect(page).to have_content('Sinatra OK')
   end
 
-  #
+  # verifies that the results page includes the method output number
   it('returns the expected count when passed a parameter string where matching words include mixed case and punctuation') do
     visit('/')
     fill_in('search_word', :with => 'a_string')
     fill_in('search_in', :with => 'A_string, a_string, - my kingdom for A_STRING!')
     click_button('Send')
     expect(page).to have_content("3")
+  end
+
+  # verifies that the results page includes the search word
+  it('has the word to e seach for on the page') do
+    visit('/')
+    fill_in('search_word', :with => 'a_string')
+    fill_in('search_in', :with => 'A_string, a_string, - my kingdom for A_STRING!')
+    click_button('Send')
+    expect(page).to have_content("a_string")
+  end
+
+  # verifies that the results page includes the input that was searched
+  it('has the string to be searched on the page') do
+    visit('/')
+    fill_in('search_word', :with => 'a_string')
+    fill_in('search_in', :with => 'A_string, a_string, - my kingdom for A_STRING!')
+    click_button('Send')
+    expect(page).to have_content('A_string, a_string, - my kingdom for A_STRING!')
   end
 
 end
