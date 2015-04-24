9d6ae4d79a4066c1bcb2efa75e6d626eb0ba1067
Drew <drew@finstrom.us>
2015-04-24 10:01:00 -0700

    add unit test to account for two instances of a matching word in the parameter

--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -2,7 +2,14 @@ require('rspec')
 require('./lib/wordcount')
 
 describe('#String.exists_number_of_times_in()') do
+  #single word test
   it("returns a one-count when passed an equal word") do
     expect(("a_string").exists_number_of_times_in("a_string")).to(eq(1))
   end
+
+  #two word test
+  it("returns a two-count when passed the same word twice") do
+    expect(("a_string").exists_number_of_times_in("a_string a_string")).to(eq(2))
+  end
+
 end
