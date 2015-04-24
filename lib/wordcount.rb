3d26c0eba72cefbbccfb4804cf86c51c6caac68a
Drew <drew@finstrom.us>
2015-04-24 10:09:43 -0700

    update method code to pass two-word unit test

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -2,12 +2,20 @@ class String
   #method to count the # of times this string exists in the parameter string
   define_method(:exists_number_of_times_in) do |sentance_to_search|
     word_to_locate = self
+    words_to_compare = sentance_to_search.split(" ")
+    matches = 0
 
-
-    if word_to_locate == sentance_to_search
-      return 1
+    #loop array of words to compare and increment matches on positive hits
+    words_to_compare.each do |word_to_compare|
+      if word_to_locate == word_to_compare
+        matches += 1
+      end
     end
 
+    # if word_to_locate == sentance_to_search
+    #   return 1
+    # end
 
+    return matches
   end
 end
